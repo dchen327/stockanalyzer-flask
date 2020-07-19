c3c3998b92f4b7e03c8530694db034ad585c1940
Daniel Li <dan.li2010@hotmail.com>
2020-07-19 15:47:30 -0400

    added comments

--- app/title_crawler.py
+++ app/title_crawler.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 import requests as rq
 import argparse as ap
 
+# takes an argument for the stock ticker and inserts it into the RobinHood URL
 parser = ap.ArgumentParser()
 parser.add_argument('-t', '--ticker', type=str, required=True,
                     help="Desired ticker to display article titles for")
@@ -18,10 +19,13 @@ url = f'https://robinhood.com/stocks/{ticker}'
 
 def fetch_article_titles(url):
     try:
+        # gets raw article data
         article = rq.get(url, timeout=5)
         article_content = BeautifulSoup(article.content, "lxml")
+        # hardcoded to find article titles specific to RobinHood; uses unique tags
         titles = article_content.find_all(
             "h3", class_="_1mENhHqLzNE3Fc8Lm0t1of")
+        # sometimes it returns a None article title, idk why
         news_titles = [title.text for title in titles if title.text != None]
         return news_titles
     except:

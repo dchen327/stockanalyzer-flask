7e1c6ec77a479344a8bc04664362724e4966aee3
David Chen <davidc3287@gmail.com>
2020-07-27 11:15:30 -0400

    remove command line arguments, clean up possible errors

--- app/title_crawler.py
+++ app/title_crawler.py
@@ -1,37 +1,33 @@
 '''
 Simple Python script that fetches RobinHood article names for stock tickers
 
-by Daniel Li
+Author: Daniel Li
 '''
 
+import requests
 from bs4 import BeautifulSoup
-import requests as rq
-import argparse as ap
-
-# takes an argument for the stock ticker and inserts it into the RobinHood URL
-parser = ap.ArgumentParser()
-parser.add_argument('-t', '--ticker', type=str, required=True,
-                    help="Desired ticker to display article titles for")
-args = vars(parser.parse_args())
-ticker = args['ticker']
-url = f'https://robinhood.com/stocks/{ticker}'
 
 
-def fetch_article_titles(url):
+def fetch_article_titles(ticker):
     try:
         # gets raw article data
-        article = rq.get(url, timeout=5)
+        url = f'https://robinhood.com/stocks/{ticker}'
+        article = requests.get(url, timeout=5)
         article_content = BeautifulSoup(article.content, "lxml")
         # hardcoded to find article titles specific to RobinHood; uses unique tags
         titles = article_content.find_all(
             "h3", class_="_1mENhHqLzNE3Fc8Lm0t1of")
         # sometimes it returns a None article title, idk why
-        news_titles = [title.text for title in titles if title.text != None]
+        news_titles = [title.text for title in titles if title.text is not None]
+        if news_titles == []:  # no titles found
+            news_titles.append(f'No articles found for ticker {ticker}')
         return news_titles
-    except:
-        pass
+    except Exception as e:
+        print(e)
+
 
 if __name__ == '__main__':
-    titles = fetch_article_titles(url)
+    ticker = 'AMZN'
+    titles = fetch_article_titles(ticker)
     for title in titles:
         print(title)

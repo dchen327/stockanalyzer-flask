36193125d94ec606643e04da9487088c5d1a9625
Daniel Li <dan.li2010@hotmail.com>
2020-07-30 13:11:37 -0400

    test a broken aspect

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,20 @@
 from flask import render_template, request
 from app import app
+from ticker_analyzer import ticker_analyzer
 @app.route('/',methods=['GET','POST'])
 
 @app.route('/index',methods=['GET','POST'])
 def index():
     if request.method == 'POST':
         text = request.form['ticker']
-        return render_template('index.html',ticker=text)
+        score = ticker_analyzer(text)
+        return render_template('index.html',score=score)
     else:
         return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
     
 

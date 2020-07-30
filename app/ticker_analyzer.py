9a57c48f769143ebaf1f44f88f442d426d55c909
David Chen <davidc3287@gmail.com>
2020-07-30 12:55:21 -0400

    add ticker_analyzer function

---
+++ app/ticker_analyzer.py
@@ -0,0 +1,19 @@
+from sentiment_analyzer import SentimentAnalyzer
+from title_crawler import TitleCrawler
+
+
+def ticker_analyzer(ticker):
+    """ Return an average sentiment and magnitude for a given stock ticker """
+    title_crawler = TitleCrawler()
+    titles = title_crawler.fetch_article_titles(ticker)
+    analyzer = SentimentAnalyzer()
+    scores = []
+    for title in titles:
+        score, magnitude = analyzer.analyze(title)
+        scores.append(score * magnitude)  # interpretation of API output
+
+    return 100 * sum(scores) / len(scores)
+
+
+if __name__ == '__main__':
+    print(ticker_analyzer('AMZN'))

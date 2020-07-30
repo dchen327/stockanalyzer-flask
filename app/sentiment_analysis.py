ad62b64a490d5d39ab9135b95b96e7dc47184fca
David Chen <davidc3287@gmail.com>
2020-07-30 12:04:58 -0400

    add SentimentAnalyzer class

--- app/sentiment_analysis.py
+++ app/sentiment_analysis.py
@@ -2,16 +2,21 @@ from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 
-client = language.LanguageServiceClient()
+class SentimentAnalyzer:
 
-text = u'Hello, world!'
-document = types.Document(
-    content=text,
-    type=enums.Document.Type.PLAIN_TEXT)
+    def __init__(self):
+        self.client = language.LanguageServiceClient()
 
-# Detects the sentiment of the text
-sentiment = client.analyze_sentiment(document=document).document_sentiment
+    def analyze(self, text):
+        """ Return a tuple of sentiment score (-1, 1) and sentiment magnitude (0, inf) """
+        self.document = types.Document(
+            content=text,
+            type=enums.Document.Type.PLAIN_TEXT
+        )
+        sentiment = self.client.analyze_sentiment(document=self.document).document_sentiment
+        return (sentiment.score, sentiment.magnitude)
 
-print('Text: {}'.format(text))
-print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
 
+if __name__ == '__main__':
+    analyzer = SentimentAnalyzer()
+    print(analyzer.analyze('Truly an amazing experience. I loved the atmosphere and the food.'))

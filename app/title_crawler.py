c3980000deb28ea04d495c7d66b745f008b05ced
David Chen <davidc3287@gmail.com>
2020-07-30 12:26:43 -0400

    create TitleCrawler class

--- app/title_crawler.py
+++ app/title_crawler.py
@@ -1,33 +1,32 @@
-'''
-Simple Python script that fetches RobinHood article names for stock tickers
-
-Author: Daniel Li
-'''
-
+"""
+The TitleCrawler class provides methods to grab article titles on Robinhood relating to a
+provided stock ticker.
+"""
 import requests
 from bs4 import BeautifulSoup
 
 
-def fetch_article_titles(ticker):
-    try:
-        # gets raw article data
-        url = f'https://robinhood.com/stocks/{ticker}'
-        article = requests.get(url, timeout=5)
-        article_content = BeautifulSoup(article.content, "lxml")
+class TitleCrawler:
+    def fetch_article_titles(self, ticker):
+        """ 
+        Given a stock ticker, return a list of article titles from Robinhood.
+        Will return an empty list if no articles found (e.g. invalid ticker)
+        """
+        url = f'https://robinhood.com/stocks/{ticker}'  # url with info
+        resp = requests.get(url, timeout=5)
+        soup = BeautifulSoup(resp.content, 'lxml')
         # hardcoded to find article titles specific to RobinHood; uses unique tags
-        titles = article_content.find_all(
-            "h3", class_="_1mENhHqLzNE3Fc8Lm0t1of")
-        # sometimes it returns a None article title, idk why
+        titles = soup.find_all(
+            'h3', class_='_1mENhHqLzNE3Fc8Lm0t1of')
+        # sometimes returned article title is None
         news_titles = [title.text for title in titles if title.text is not None]
-        if news_titles == []:  # no titles found
-            news_titles.append(f'No articles found for ticker {ticker}')
+
         return news_titles
-    except Exception as e:
-        print(e)
 
 
 if __name__ == '__main__':
     ticker = 'AMZN'
-    titles = fetch_article_titles(ticker)
+    title_crawler = TitleCrawler()
+    titles = title_crawler.fetch_article_titles(ticker)
     for title in titles:
         print(title)

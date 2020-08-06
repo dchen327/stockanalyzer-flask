6635e22f2f99e39fa995ae3a0283723fd46d3d5f
David Chen <davidc3287@gmail.com>
2020-08-06 14:24:10 -0400

    add sentiment page, separate from index

--- app/routes.py
+++ app/routes.py
@@ -3,17 +3,21 @@ from app import app
 from app.ticker_analyzer import ticker_analyzer
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index.html')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/sentiment.html', methods=['GET', 'POST'])
+def sentiment():
     if request.method == 'POST':
         text = request.form['ticker']
         score = ticker_analyzer(text)
-        return render_template('index.html', score=score, ticker=text)
+        return render_template('sentiment.html', score=score, ticker=text)
     else:
-        return render_template('index.html')
+        return render_template('sentiment.html')
 
 
-@app.route('/about')
+@app.route('/about.html')
 def about():
     return render_template('about.html')
